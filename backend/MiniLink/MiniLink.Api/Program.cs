08412370f96cbe4b44a749d36f77686d6948a2c5
João Oliveira <jvitoroliveira0711@gmail.com>
2024-10-20 17:55:55 -0300

    app(#1): Configura a conexão com banco de dados e migração de banco de dados

--- backend/MiniLink/MiniLink.Api/Program.cs
+++ backend/MiniLink/MiniLink.Api/Program.cs
@@ -1,15 +1,34 @@
+using FluentMigrator.Runner;
+using Microsoft.EntityFrameworkCore;
+using MiniLink.Infrastructure.Context;
+
 var builder = WebApplication.CreateBuilder(args);
 
-// Add services to the container.
-
 builder.Services.AddControllers();
-// Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
 
+var applicationConnectionString = Environment.GetEnvironmentVariable("SQL_CONN_APPLICATION")
+                                  ?? throw new InvalidOperationException("SQL_CONN_APPLICATION not found");
+var migrationConnectionString = Environment.GetEnvironmentVariable("SQL_CONN_MIGRATION")
+                                ?? throw new InvalidOperationException("SQL_CONN_MIGRATION not found");
+
+builder.Services.AddDbContext<ApplicationDbContext>(o => o.UseNpgsql(applicationConnectionString));
+builder.Services.AddFluentMigratorCore()
+    .ConfigureRunner(rb => rb
+        .AddPostgres()
+        .WithGlobalConnectionString(migrationConnectionString)
+        .ScanIn(typeof(MiniLink.Infrastructure.Migrations.V001_CreateLinksSequence).Assembly).For.Migrations())
+    .AddLogging(lb => lb.AddFluentMigratorConsole());
+
 var app = builder.Build();
 
-// Configure the HTTP request pipeline.
+using (var scope = app.Services.CreateScope())
+{
+    var migrationRunner = scope.ServiceProvider.GetRequiredService<IMigrationRunner>();
+    migrationRunner.MigrateUp();
+}
+
 if (app.Environment.IsDevelopment())
 {
     app.UseSwagger();
@@ -17,9 +36,6 @@ if (app.Environment.IsDevelopment())
 }
 
 app.UseHttpsRedirection();
-
 app.UseAuthorization();
-
 app.MapControllers();
-
 app.Run();
