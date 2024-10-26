a6415629a7161abea4e1e6e0eceea33bd43616a1
João Oliveira <jvitoroliveira0711@gmail.com>
2024-10-26 14:04:31 -0300

    app(#1): Adiciona anotações de validação no DTO

--- backend/MiniLink/MiniLink.Domain/Dtos/Link/ShortenLinkDto.cs
+++ backend/MiniLink/MiniLink.Domain/Dtos/Link/ShortenLinkDto.cs
@@ -1,6 +1,10 @@
+using System.ComponentModel.DataAnnotations;
+
 namespace MiniLink.Domain.Dtos.Link;
 
 public class ShortenLinkDto
 {
+    [Required(ErrorMessage = "Informe a URL.")]
+    [Url(ErrorMessage = "A URL deve ser um endereço válido.")]
     public string OriginalUrl { get; set; }
 }
